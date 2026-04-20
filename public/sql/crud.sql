dec1cdafa58355991bb29b3ee668e72b951d2582
cristianmotta <cdavidg4396@gmail.com>
2026-04-20 10:31:25 -0500

    sql

--- public/sql/crud.sql
+++ public/sql/crud.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-04-2026 a las 05:41:25
+-- Tiempo de generación: 20-04-2026 a las 17:30:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `crud`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id`, `nombre`) VALUES
+(1, 'Tecnología'),
+(2, 'Ropa'),
+(3, 'Hogar');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(150) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `id_categoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `nombre`, `precio`, `id_categoria`) VALUES
+(1, 'Laptop', 2500.00, 1),
+(3, 'Sofá', 900.00, 3),
+(7, 'Teclado', 300.00, 1);
 
 -- --------------------------------------------------------
 
@@ -42,14 +84,28 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`id`, `nombre`, `correo`, `password`, `fech_nac`, `activo`, `creado_en`) VALUES
-(2, 'Carlos Lopez', 'carlosLopez@email.com', '123456', '1998-11-25', 1, '2026-04-11 22:02:20'),
 (3, 'Maria Ruiz', 'mariaR@email.com', '123456', '2001-03-08', 1, '2026-04-11 22:02:20'),
 (4, 'paula', 'paula@gmail.com', '$2y$10$IL108MCCUbE2vbXTdkCr.eDOnPtqkSit02gwqNVUY81MJcWrMWsvK', '2006-10-31', 1, '2026-04-12 06:16:33'),
-(6, 'Andres za', 'Andres@email.com', '$2y$10$3b.4bGvDuJR.R2WOks3YMucJgTUq/cyp27YBVPBQ.syDfWNN6h1G2', '2003-10-15', 1, '2026-04-13 03:16:42');
+(6, 'Andres za', 'Andres@email.com', '$2y$10$3b.4bGvDuJR.R2WOks3YMucJgTUq/cyp27YBVPBQ.syDfWNN6h1G2', '2003-10-15', 1, '2026-04-13 03:16:42'),
+(24, 'cristian', 'cris@gmail.com', '$2y$10$E9IXG5yF1LgCGUiFRQXXvewfE10UDP0.OWfYFH1VYO3bcLLyCuPyy', '1995-06-20', 1, '2026-04-20 01:21:39'),
+(25, 'Juan', 'Juan@email.com', '$2y$10$.uZ1AH0YNE.p54uOJCtEr.nz6zQJplsUvFF9SBnnQrRgavPNYkFjO', '2026-04-14', 1, '2026-04-20 04:15:38');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `productos` (`id_categoria`);
 
 --
 -- Indices de la tabla `usuario`
@@ -63,10 +119,32 @@ ALTER TABLE `usuario`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD CONSTRAINT `productos` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
